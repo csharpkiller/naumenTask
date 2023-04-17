02809bbbefa5d476ab881f3161333fcbea9b19ed
csharpkiller <66179446+csharpkiller@users.noreply.github.com>
2023-04-17 15:16:07 +0500

    Add files via upload

---
+++ src/main/java/ru/oop/Underground.java
@@ -0,0 +1,26 @@
+package ru.oop;
+
+public class Underground implements Transport{
+    public Underground(Person person){
+
+    }
+    @Override
+    public Position getPosition() {
+        return null;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+
+    }
+
+    @Override
+    public void moveTo(Position position) {
+
+    }
+
+    @Override
+    public Position getActualPersonPosition() {
+        return null;
+    }
+}
